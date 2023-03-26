3346f93a825804fe4c2195797fc27aabcbbcbfc2
Conor <lacech21@wfu.edu>
2023-03-26 15:15:37 -0400

    .2.csv deleted

--- PrimarySims.R
+++ PrimarySims.R
@@ -154,6 +154,6 @@ for (i in 1:500){
   }
 }
 
-write.csv(dat_dMACS, file = "dat_dMACS.2.csv")
-write.csv(dat_dMACS_S, file = "dat_dMACS_S.2.csv")
+write.csv(dat_dMACS, file = "dat_dMACS.csv")
+write.csv(dat_dMACS_S, file = "dat_dMACS_S.csv")
 
